c7217dbdcbf687c5d37c7f3201b2b81bb84eae30
Ayushman Singh <ayuams2008@gmail.com>
2026-07-08 02:14:34 +0530

    meme api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Form from "./components/form";
 function App() {
   const [count, setCount] = useState(0);
-
+  const [meme, setMeme] = useState();
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((dat) => setMeme(dat.data.memes));
+  }, []);
+  let onClick = (e) => {
+    e.preventDefault();
+    setCount((prev) => prev + 1);
+  };
   return (
     <>
       <Header />
-      <Form />
+      <Form img={meme ? meme[count] : ""} handleClick={onClick} />
     </>
   );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,17 +1,22 @@
-export default function () {
+import meme from "../assets/meme.png";
+
+export default function (props) {
   return (
-    <form className="memeform">
-      <label htmlFor="toptext">
-        Top Text
-        <input type="text" placeholder="Stayed Home" className="toptext" />
-      </label>
-      <label htmlFor="bottext">
-        Bottom Text
-        <input type="text" placeholder="Saved Lives" className="bottext" />
-      </label>
-      <button type="submit" className="generate">
-        Generate Meme
-      </button>
-    </form>
+    <>
+      <form className="memeform">
+        <label htmlFor="toptext">
+          Top Text
+          <input type="text" placeholder="Stayed Home" className="toptext" />
+        </label>
+        <label htmlFor="bottext">
+          Bottom Text
+          <input type="text" placeholder="Saved Lives" className="bottext" />
+        </label>
+        <button type="submit" className="generate" onClick={props.handleClick}>
+          Generate Meme
+        </button>
+      </form>
+      <img src={props.img.url} alt="memeimage" className="memeimage" />
+    </>
   );
 }
